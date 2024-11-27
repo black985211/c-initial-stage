ec9965f44abb1acbff8e92528de08ddd2940ce16
roaring-black-fertilizer <1766155164@qq.com>
2024-11-27 23:19:07 +0800

    3-11_20

---
+++ Test3-11_20/Test3-11_20/test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int Add(int x, int y) {
+	return (x + y);
+}
+
+
+int main() {
+
+	int arr[10]={ 1,2,3,4,5,6,7,8,9,10 };
+	printf("%d", arr[2]);
+	return 0;
+	/*int c = Add(20, 30);
+	printf("%d", c);
+	return 0;*/
+	/*int iq = 0;
+	while (scanf("%d",&iq)==1) {
+		if (iq >= 140) {
+			printf("Genius\n");
+		}
+
+	}*/
+
+	/*int a = 40;
+	int c = 212;
+	int res = 0;
+	res = (-8 + 22) * a - 10 + c / 2;
+	printf("%d\n" ,& res);*/
+
+	/*int M = 0;
+	printf("请输入1-100000之间的一个整数\n");
+	scanf("%d", &M);
+	if (M < 1 || M>100000) {
+		printf("请重新输入");
+
+	}
+	else if (M % 5 == 0) {
+		printf("Yes\n");
+	}
+	else {
+		printf("No\n");
+	}
+
+
+	return 0;*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
