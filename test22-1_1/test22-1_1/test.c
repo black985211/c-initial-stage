767b3f1f52fce9d922801a3f7c1779d0f06bcd6a
roaring-black-fertilizer <1766155164@qq.com>
2025-01-02 20:30:50 +0800

    1.1

--- test22-1_1/test22-1_1/test.c
+++ test22-1_1/test22-1_1/test.c
@@ -34,39 +34,42 @@
 
 //ÉÏÈı½Ç¾ØÕóÅĞ¶¨
 
-int main()
-{
-	int n = 0;
-	scanf("%d", &n);
-	int arr[10][10];
-	int i = 0;
-	
-	for (i = 0;i < n;i++)
-	{
-		int j = 0;
-		for (j = 0;j < n;j++)
-		{
-			scanf("%d", &arr[i][j]);
-		}
-	}
-	int flag = 1;
-	for (i = 0;i < n;i++)
-	{
-		int j = 0;
-		for (j = 0;j < i;j++)
-		{
-			if (arr[i][j] == 0)
-			{
-				flag = 0;
-				goto end;
-			}
-		}
-	}
-end:
-	if (flag == 1)
-		printf("Yes");
-	else if (flag == 0)
-		printf("No");
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[10][10];
+//	int i = 0;
+//	
+//	for (i = 0;i < n;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < n;j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	int flag = 1;
+//	for (i = 0;i < n;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < i;j++)
+//		{
+//			if (arr[i][j] == 0)
+//			{
+//				flag = 0;
+//				goto end;
+//			}
+//		}
+//	}
+//end:
+//	if (flag == 1)
+//		printf("Yes");
+//	else if (flag == 0)
+//		printf("No");
+//
+//
+//}
 
 
-}
+
